bd0cddfd4ada65ce5334d7bc717d4aa4345b428c
raulpC <raul.pineda.caballero@gmail.com>
2023-05-19 00:11:06 +0300

    testing funtions + - * /*

--- forth/lib/forth.dart
+++ forth/lib/forth.dart
@@ -7,76 +7,61 @@ class Forth {
     this.definitions = [];
   }
   // Put your code here
-  void evaluate(String stack) {}
-}
+  void evaluate(String input) {
 
-void main() {
-  /**
-   * Crear el objero stack
-   */
-  // ignore: unused_local_variable
-  Forth forth = new Forth();
-  // ignore: unused_local_variable
-  //List<String> stack = [];
-  // ignore: unused_local_variable
-  String input = '1 2 +'; // input'datas of stack
-  // ignore: unused_local_variable
-  List<String> datas = input.split(' ');
-
-  print(datas);
-
-  /**
-   * comprobar cada dato
-   */
-  int result;
-  for (var i = datas.length; i > 0; i--) {
-    switch (datas[i - 1]) {
+    // ignore: unused_local_variable
+    List<String> datas = input.split(' ');
+    
+    int result;
+  for (var i = 0; i < datas.length; i++) {
+    switch (datas[i]) {
       case '+':
         print('+');
-
-        if (forth.stack.length >= 2) {
-          result = forth.stack[forth.stack.length - 2] +
-              forth.stack[forth.stack.length - 1];
-          forth.stack.removeAt(forth.stack.length - 1);
-          forth.stack.removeAt(forth.stack.length - 1);
-          forth.stack.add(result);
+        if(stack.length < 2) throw Exception('Stack empty');
+        if (stack.length >= 2 ) {
+          result = stack[stack.length - 2] +
+              stack[stack.length - 1];
+          stack.removeAt(stack.length - 1);
+          stack.removeAt(stack.length - 1);
+          stack.add(result);
         } else {
           print('El stack no tiene suficiente datos');
         }
         break;
       case '-':
         print('-');
-
-        if (forth.stack.length >= 2) {
-          result = forth.stack[forth.stack.length - 2] -
-              forth.stack[forth.stack.length - 1];
-          forth.stack.removeAt(forth.stack.length - 1);
-          forth.stack.removeAt(forth.stack.length - 1);
-          forth.stack.add(result);
+        if(stack.length < 2) throw Exception('Stack empty');
+        if (stack.length >= 2) {
+          result = stack[stack.length - 2] -
+              stack[stack.length - 1];
+          stack.removeAt(stack.length - 1);
+          stack.removeAt(stack.length - 1);
+          stack.add(result);
         } else {
           print('El stack no tiene suficiente datos');
         }
         break;
       case '/':
         print('/');
-
-        if (forth.stack.length >= 2) {
-          result = int.fromEnvironment((forth.stack[forth.stack.length - 2] /
-                  forth.stack[forth.stack.length - 1])
-              .toString());
-          forth.stack.removeAt(forth.stack.length - 1);
-          forth.stack.removeAt(forth.stack.length - 1);
+        if(stack.length < 2) throw Exception('Stack empty');
+        if(stack[stack.length - 1] == 0) throw Exception('Division by zero');
+        if (stack.length >= 2) {
+          result = stack[stack.length - 2] ~/ stack[stack.length - 1];
+              print('resul: $result');
+          stack.removeAt(stack.length - 1);
+          stack.removeAt(stack.length - 1);
+          stack.add(result);
         }
         break;
       case '*':
         print('*');
-
-        if (forth.stack.length >= 2) {
-          result = forth.stack[forth.stack.length - 2] *
-              forth.stack[forth.stack.length - 1];
-          forth.stack.removeAt(forth.stack.length - 1);
-          forth.stack.removeAt(forth.stack.length - 1);
-          forth.stack.add(result);
+        if(stack.length < 2) throw Exception('Stack empty');
+        if (stack.length >= 2) {
+          result = stack[stack.length - 2] *
+              stack[stack.length - 1];
+          stack.removeAt(stack.length - 1);
+          stack.removeAt(stack.length - 1);
+          stack.add(result);
         } else {
           print('El stack no tiene suficiente datos');
         }
@@ -94,9 +79,107 @@ void main() {
         print('drop');
         break;
       default:
-        forth.stack.add(int.parse(datas[i - 1])); // añade el numero al stack
-        print('Stack ${forth.stack}');
+        stack.add(int.parse(datas[i])); // añade el numero al stack
+        print('Stack ${stack}');
         break;
     }
   }
+  print('Stack ${stack}');
+  }
 }
+void main(List<String> args) {
+  // ignore: unused_local_variable
+  Forth forth = new Forth();
+  forth.evaluate('+');
+}
+// void main() {
+//   /**
+//    * Crear el objero stack
+//    */
+//   // ignore: unused_local_variable
+//   Forth forth = new Forth();
+//   // ignore: unused_local_variable
+//   //List<String> stack = [];
+//   // ignore: unused_local_variable
+//   String input = '2 2 / 2 3 +'; // input'datas of stack
+//   // ignore: unused_local_variable
+//   List<String> datas = input.split(' ');
+
+//   print(datas);
+
+//   /**
+//    * comprobar cada dato
+//    */
+//   int result;
+//   for (var i = 0; i < datas.length; i++) {
+//     switch (datas[i]) {
+//       case '+':
+//         print('+');
+
+//         if (forth.stack.length >= 2) {
+//           result = forth.stack[forth.stack.length - 2] +
+//               forth.stack[forth.stack.length - 1];
+//           forth.stack.removeAt(forth.stack.length - 1);
+//           forth.stack.removeAt(forth.stack.length - 1);
+//           forth.stack.add(result);
+//         } else {
+//           print('El stack no tiene suficiente datos');
+//         }
+//         break;
+//       case '-':
+//         print('-');
+
+//         if (forth.stack.length >= 2) {
+//           result = forth.stack[forth.stack.length - 2] -
+//               forth.stack[forth.stack.length - 1];
+//           forth.stack.removeAt(forth.stack.length - 1);
+//           forth.stack.removeAt(forth.stack.length - 1);
+//           forth.stack.add(result);
+//         } else {
+//           print('El stack no tiene suficiente datos');
+//         }
+//         break;
+//       case '/':
+//         print('/');
+
+//         if (forth.stack.length >= 2) {
+//           result = forth.stack[forth.stack.length - 2] ~/ forth.stack[forth.stack.length - 1];
+//               print('resul: $result');
+//           forth.stack.removeAt(forth.stack.length - 1);
+//           forth.stack.removeAt(forth.stack.length - 1);
+//           forth.stack.add(result);
+//         }
+//         break;
+//       case '*':
+//         print('*');
+
+//         if (forth.stack.length >= 2) {
+//           result = forth.stack[forth.stack.length - 2] *
+//               forth.stack[forth.stack.length - 1];
+//           forth.stack.removeAt(forth.stack.length - 1);
+//           forth.stack.removeAt(forth.stack.length - 1);
+//           forth.stack.add(result);
+//         } else {
+//           print('El stack no tiene suficiente datos');
+//         }
+//         break;
+//       case 'dut':
+//         print('dut');
+//         break;
+//       case 'over':
+//         print('over');
+//         break;
+//       case 'swap':
+//         print('swap');
+//         break;
+//       case 'drop':
+//         print('drop');
+//         break;
+//       default:
+//         forth.stack.add(int.parse(datas[i])); // añade el numero al stack
+//         print('Stack ${forth.stack}');
+//         break;
+//     }
+//   }
+//   print('Stack ${forth.stack}');
+// }
